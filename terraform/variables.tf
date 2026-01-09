b010645edf67b8239fb9c0d1b1fede24fad93c03
jhayashi1 <jaredhayashi@gmail.com>
2026-01-09 13:11:20 -0600

    chore: default variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,14 +7,17 @@ variable "domain" {
 variable "git_url" {
   description = "Git repository URL"
   type        = string
+  default     = "https://github.com/jhayashi1/spt-web-quest-builder"
 }
 
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "spt-web-quest-builder"
 }
 
 variable "bucket_name" {
   description = "Name of the S3 bucket for website hosting"
   type        = string
+  default     = "spt-web-quest-builder"
 }
